512cfa430c403179d11bcef9c6dc07ed4efaed39
Босс <bs_lexx@mail.ru>
2022-02-17 03:25:25 +0500

    Initial Commit

---
+++ src/main/java/geekbrains/com/Constants.java
@@ -0,0 +1,6 @@
+package geekbrains.com;
+
+public class Constants {
+    private final static String SERVER_ADDRESS = "localhost";
+    private final static int SERVER_PORT = 8080;
+}
---
+++ src/main/java/geekbrains/com/ServerConsole.java
@@ -0,0 +1,11 @@
+package geekbrains.com;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class ServerConsole {
+}
